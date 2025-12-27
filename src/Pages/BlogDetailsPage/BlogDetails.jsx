6371ba85c9242cbcf1192cd9ef01b2aa0eb0a812
iamamolsarker <amolsarker17@gmail.com>
2025-12-27 17:33:21 +0600

    blog details

--- src/Pages/BlogDetailsPage/BlogDetails.jsx
+++ src/Pages/BlogDetailsPage/BlogDetails.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const BlogDetails = () => {
+    const {id} = useParams();
+    const bookId = parseInt(id)
+    const data = useLoaderData();
+    const singleBlog = data.find(book => book.id === bookId)
+    const {title, author, content} = singleBlog;
     return (
-        <div className='text-white'>
-            blog details
+        <div className='max-w-7xl mx-auto px-4 m-auto flex gap-4 py-20 mt-20 flex-wrap'>
+            <h2 className='font-semibold text-2xl'>{title}</h2>
+            <p>{content}</p>
+            <p>written by: {author}</p>
         </div>
     );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -30,6 +30,7 @@ export const router = createBrowserRouter([
         },
         {
           path:"single-blog/:id",
+          loader: ()=>fetch('data.json'),
           Component: BlogDetails
         }
     ]
